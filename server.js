550d3d37a9cf50055b6de9effd7b4343813df3a0
Alex Olshansky <alex@creagenics.com>
2021-04-15 23:45:13 +0200

    Enable gzip compression

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import fastify from 'fastify';
 import fastifyCors from 'fastify-cors';
+import fastifyCompress from 'fastify-compress';
 import { loadAndFormat } from './data-formatters.js';
 
 const app = fastify();
@@ -7,6 +8,8 @@ const app = fastify();
 app.register(fastifyCors, {
   origin: true, // reflect the request origin
 });
+
+app.register(fastifyCompress, { encodings: ['gzip'] }); // gzip is twice faster than br on this low-end server
 
 app.get('/homepage', async request => {
   return loadAndFormat(`https://freefeed.net/v2/timelines/home?offset=0`, request.headers.authorization);
